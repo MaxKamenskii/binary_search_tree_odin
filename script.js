cf8dd10c93dcdc491951587e363e989eaea12772
Max Dendy <80255339+MaxKamenskii@users.noreply.github.com>
2025-12-29 23:04:02 +0500

    implement insert function

--- script.js
+++ script.js
@@ -1,85 +1,96 @@
 class Node {
-    constructor(data, left = null, right = null) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Tree {
-    constructor(arr) {
-        const sortedSet = [...new Set(arr)].sort((a, b) => a - b);
-        this.root = this.buildTree(sortedSet)
+  constructor(arr) {
+    const sortedSet = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedSet);
+  }
+
+  buildTree(array) {
+    // base case
+    if (array.length === 0) {
+      return null;
     }
 
-    buildTree(array) {
-        // base case
-        if (array.length === 0) {
-            return null
+    const midIndex = Math.floor(array.length / 2);
+    const root = new Node(array[midIndex]);
+
+    const leftHalf = array.slice(0, midIndex);
+    const rightHalf = array.slice(midIndex + 1);
+
+    root.left = this.buildTree(leftHalf);
+    root.right = this.buildTree(rightHalf);
+
+    return root;
+  }
+
+  insert(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
         }
-
-        const midIndex = Math.floor(array.length / 2);
-        const root = new Node(array[midIndex]);
-
-        const leftHalf = array.slice(0, midIndex);
-        const rightHalf = array.slice(midIndex + 1);
-
-        root.left = this.buildTree(leftHalf);
-        root.right = this.buildTree(rightHalf);
-
-        return root
+      };
+      return searchTree(node);
     }
+  }
 
-    insert(node) {
-        if(this.root == null) {
-            this.root = node;
-        }
-    }
+  deleteItem() {}
 
-    deleteItem() {
+  find() {}
 
-    }
+  leverOrderForEach(callback) {}
 
-    find() {
+  inOrderForEach(callback) {}
 
-    }
+  preOrderForEach(callback) {}
+  postOrderForEach(callback) {}
+  height() {}
+  depth() {}
+  isBalanced() {}
+  rebalance() {}
+}
 
-    leverOrderForEach(callback) {
+let theArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// let unique = [...new Set(theArray)];
+// let sorted = unique.sort(function (a, b) {
+//   return a - b;
+// });
 
-    }
+// // 1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345
 
-    inOrderForEach(callback) {
+// let middle = sorted[Math.round((sorted.length - 1) / 2)];
 
-    }
+// console.log(
+//   unique.sort(function (a, b) {
+//     return a - b;
+//   })
+// );
+// console.log(middle);
 
-    preOrderForEach(callback) {
-
-    }
-    postOrderForEach(callback) {
-
-    }
-    height() {
-
-    }
-    depth() {
-
-    }
-    isBalanced() {
-
-    }
-    rebalance() {
-
-    }
-};
-
-
-let theArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-let unique = [...new Set(theArray)]
-let sorted = unique.sort(function(a, b){return a - b})
-
-// 1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345
-
-let middle = sorted[Math.round((sorted.length - 1) / 2)];
-
-console.log(unique.sort(function(a, b){return a - b}))
-console.log(middle)
+let myTree = new Tree(theArray);
+console.log(myTree);
